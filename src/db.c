c2ddb3cf3cc69bf80a44ce4ad2ec7de2c9aad1a2
Paul Barnetta <paul.barnetta@gmail.com>
2019-06-04 16:05:36 +1200

    Add minizen_db_get_path function
    This can be used for returning a path relative to the database's data
    directory. It will be used in an upcoming commit to resolve paths to
    JSON data files, for which we expect to use varargs-style calls; e.g.:
    
    	minizen_db_get_path(db, "%s.json", ...)
    
    which saves us from having to deal with another string concatenation.
    
    As noted in the comments this is probably an overkill; but the common
    alternative:
    
    	char path[PATH_MAX];
    	snprintf(path, PATH_MAX, "%s/%s", db->dir, filename);
    
    can leave us with a truncated path; see:
    http://insanecoding.blogspot.com/2007/11/pathmax-simply-isnt.html

--- src/db.c
+++ src/db.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -31,6 +32,38 @@ struct minizen_db * minizen_db_open(const char *dir) {
 	}
 
 	return db;
+}
+
+
+/**
+ * Qualifies a filename that is relative to the database's data directory. This
+ * is probably an overkill (given PATH_MAX will *likely* always be long enough)
+ * but we also take the opportunity to support varargs in the filename as a
+ * convenience for callers.
+ */
+static char * minizen_db_get_path(struct minizen_db *db,
+	const char *filename, ...) {
+
+	int dir_len = strlen(db->dir);
+
+	va_list ap;
+	va_start(ap, filename);
+	int bytes = dir_len + 1 + vsnprintf(NULL, 0, filename, ap) + 1;
+	va_end(ap);
+
+	char *path = malloc(bytes);
+	if (! path) {
+		return NULL;
+	}
+
+	strcpy(path, db->dir);
+	path[dir_len] = '/';
+
+	va_start(ap, filename);
+	vsnprintf(path + dir_len + 1, bytes - dir_len - 1, filename, ap);
+	va_end(ap);
+
+	return path;
 }
 
 
