63853a21d1297c01f767e7787d6c2bc5bcdde5d4
Paul Barnetta <paul.barnetta@gmail.com>
2019-06-05 09:09:18 +1200

    Output search results in a "human readable format"
    Well, arguably in a slightly more human readable format ;-)
    
    This is a step up from raw JSON though; and more importantly it lays the
    foundation for including related objects in a future commit.

--- src/minizen-search.c
+++ src/minizen-search.c
@@ -1,6 +1,50 @@
-#include <unistd.h>
+#include <stdio.h>
 
+#include "formatter.h"
 #include "minizen.h"
+
+
+static int print_primary_header(struct minizen_db *db, int results) {
+
+	/*
+	 * Don't leave the user without any output if there are no results (and
+	 * so print_primary_object_header isn't called).
+	 */
+	if (! results) {
+		fprintf(stderr, "No results found\n");
+	}
+
+	return 0;
+}
+
+
+static int print_primary_object_header(struct minizen_db *db,
+	json_object *json, int i) {
+
+	if (i > 1) {
+		printf("--\n");
+	}
+
+	printf("Result %d:\n", i);
+
+	return 0;
+}
+
+
+static void print_kv_pair(const char *key, json_object *value) {
+
+	printf("%20s: %s\n", key, json_object_get_string(value));
+}
+
+
+static int print_object(struct minizen_db *db, json_object *object, int i) {
+
+	json_object_object_foreach(object, key, value) {
+		print_kv_pair(key, value);
+	}
+
+	return 0;
+}
 
 
 int minizen_search(struct minizen_db *db, const char *table, const char *key,
@@ -11,10 +55,15 @@ int minizen_search(struct minizen_db *db, const char *table, const char *key,
 		return EXIT_FAILURE;
 	}
 
-	json_object_to_fd(STDOUT_FILENO, results, JSON_C_TO_STRING_PRETTY);
+	struct minizen_af_data data;
+	data.header = print_primary_header;
+	data.object.header = print_primary_object_header;
+	data.object.body = print_object;
+
+	int rc = minizen_array_formatter(db, results, &data);
 
 	json_object_put(results);
 
-	return EXIT_SUCCESS;
+	return rc ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
